f57f1194cef62799eea559cde70562d84705a741
xuty <xty50337@hotmail.com>
2025-02-22 22:52:51 +0800

    Add hot reload

--- Package.swift
+++ Package.swift
@@ -2,20 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "hello",
+    name: "VaporHotReload",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v14)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // 🔥 A Swift package for reloading code at runtime
+        .package(url: "https://github.com/ShaftUI/SwiftReload.git", branch: "main"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
+                "SwiftReload",
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
@@ -28,13 +31,36 @@ let package = Package(
                 .target(name: "App"),
                 .product(name: "VaporTesting", package: "vapor"),
             ],
-            swiftSettings: swiftSettings
-        )
+            swiftSettings: swiftSettings,
+            linkerSettings: linkerSettings
+        ),
     ],
     swiftLanguageModes: [.v5]
 )
 
-var swiftSettings: [SwiftSetting] { [
-    .enableUpcomingFeature("DisableOutwardActorInference"),
-    .enableExperimentalFeature("StrictConcurrency"),
-] }
+var swiftSettings: [SwiftSetting] {
+    [
+        .enableUpcomingFeature("DisableOutwardActorInference"),
+        .enableExperimentalFeature("StrictConcurrency"),
+        // Enable private imports for hot reload
+        .unsafeFlags(
+            ["-Xfrontend", "-enable-private-imports"],
+            .when(configuration: .debug)
+        ),
+        // Enable implicit dynamic for hot reload
+        .unsafeFlags(
+            ["-Xfrontend", "-enable-implicit-dynamic"],
+            .when(configuration: .debug)
+        ),
+    ]
+}
+
+var linkerSettings: [LinkerSetting] {
+    [
+        // Export dynamic symbols for hot reload
+        .unsafeFlags(
+            ["-Xlinker", "--export-dynamic"],
+            .when(platforms: [.linux, .android], configuration: .debug)
+        )
+    ]
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,17 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+    app.get(use: home)
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+    app.get("hello", use: hello)
+}
+
+@Sendable
+func home(_ req: Request) async throws -> String {
+    return "It works!"
+}
+
+@Sendable
+func hello(_ req: Request) async throws -> String {
+    return "Hello, world!"
 }
